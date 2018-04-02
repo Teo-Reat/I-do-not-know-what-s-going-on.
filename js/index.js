a4aae2b502778303104f6e827969c0891f4ee36f
Teo Reat <teo.reat@gmail.com>
2018-04-02 17:25:56 +0300

    Added button, fixes and small changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 var mainNavMenu = document.querySelector('.hidden-block');
 var mainNavToggle = document.querySelector('.main-nav_toggle');
+var mainNavClose = document.querySelector('.menu-close');
 
 mainNavToggle.addEventListener('click', function (event) {
     event.preventDefault();
     mainNavMenu.classList.toggle('hidden-block');
+});
+
+mainNavClose.addEventListener('click', function () {
+    mainNavMenu.classList.add('hidden-block');
+    console.log('Blya');
 });
